06af24f1a43d8c4da4113c0c595c3aea54253eb1
Karine Vardanyan <mko22>
2020-01-29 18:41:20 +0400

    loops

---
+++ m.py
@@ -0,0 +1,17 @@
+first_imput = int(input("Enter a number"))
+for i in range(1, 10):
+	print(i,  "*", first_imput, "=", i * first_imput)
+
+x = "*"
+for i in range(1, 6):
+	print(i * x)
+for i in range (4, 0, -1):
+	print(i * x)
+
+user_input = int(input("Enter a number"))
+for i in range(1, user_input):
+	if i % 2 == 1:
+		print(i)
+
+
+
